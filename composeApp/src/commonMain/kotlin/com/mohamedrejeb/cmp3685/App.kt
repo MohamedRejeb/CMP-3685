994e20d236ab881f0125628793af0f7cc2271254
= <=>
2023-09-19 11:19:24 +0100

    Update Compose to 1.5.1 and add Switch

--- composeApp/src/commonMain/kotlin/com/mohamedrejeb/cmp3685/App.kt
+++ composeApp/src/commonMain/kotlin/com/mohamedrejeb/cmp3685/App.kt
@@ -1,10 +1,12 @@
 package com.mohamedrejeb.cmp3685
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.mohamedrejeb.cmp3685.theme.AppTheme
 
@@ -12,14 +14,24 @@ import com.mohamedrejeb.cmp3685.theme.AppTheme
 internal fun App(
     systemAppearance: (isLight: Boolean) -> Unit = {}
 ) = AppTheme(systemAppearance) {
+    var switch by remember {
+        mutableStateOf(false)
+    }
     Box(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        Button(
-            onClick = {}
-        ) {
-            Text("Button")
+        Row {
+            Button(
+                onClick = {}
+            ) {
+                Text("Button")
+            }
+
+            Switch(
+                checked = switch,
+                onCheckedChange = { switch = it }
+            )
         }
     }
 }
